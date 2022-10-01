83303dd95b48ef0437e45ab086cbbdefec8fb5c5
Mario Zoth <mario.zoth@icloud.com>
2022-10-01 16:08:09 +0000

    Refactor

--- app.js
+++ app.js
@@ -1,48 +1,27 @@
-const sentences = [
-  "Peter hat Gregor mit dem Ball am Kopf getroffen.",
-  "Wir brauchen noch Mehl für den Kuchen.",
-  "Sie treffen sich jede Woche bei ihren Nachbarn.",
-  "Simone weint, weil sie ihre Hose zerrissen hat.",
-  "Das Brot ist schon hart.",
-  "Lisa gießt jeden Tag in Omas Zimmer die Blumen.",
-  "Roland trifft sich morgen mit seinen Freunden.",
-  "Sei nicht so frech!",
-  "Das war wirklich eine Gemeinheit!",
-  "Mir war langweilig, da habe ich Steine in den See geworfen."
-];
-
-function toggleSentence(ev) {
-  const item = ev.target
-  if (item.nodeName.toLowerCase() !== "span") return;
-  console.info(`toggling ${item}`)
-  item.classList.toggle("hidden")
-}
-
-function writeSentences(list) {
-  if (!list) return;
-  console.info("writing sentences …")
-
-  sentences.forEach(sentence => {
-    list.insertAdjacentHTML("beforeend", `<li><span>${sentence}</span></li>`)
-    list.querySelector("li:last-child span").addEventListener("click", toggleSentence)
-  })
-
-  return list
-}
-
 function init() {
-  const list = document.querySelector("main ul")
-  writeSentences(list)
 
-  document.querySelector("header").addEventListener("click", event => {
-    const action = event && event.target && event.target.className
-    action === "bigger"
-      ? list.classList.add("big")
-      : list.classList.remove("big")
+  document.querySelector("body").addEventListener('click', event => {
+    const target = event.target
+    const action = target
+                && target.dataset
+                && target.dataset.size
+
+    // toggle font-size
+    if (action) {
+      action === 'big'
+        ? document.body.classList.add('big')
+        : document.body.classList.remove('big')
+    }
+
+
+    // hide show sentence
+    if (target.nodeName.toLowerCase() === 'li') {
+      target.classList.toggle('hidden')
+    }
   })
 }
 
-window.addEventListener("DOMContentLoaded", init)
+window.addEventListener('DOMContentLoaded', init)
 
 // deploy with:
 // surge  --domain ddkt.surge.sh
